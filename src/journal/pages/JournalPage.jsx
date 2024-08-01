1a758dcf1bc4dbaa37fc9b0cfd4695e9684b1014
Andres Guerrero <aguerrero@fi.uba.ar>
2024-08-01 14:42:49 -0300

    end jornal-app-ui

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,33 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../view"
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1' >JournalPage</Typography>
-      <MailOutline/>
-    </>
+    <JournalLayout>
+      {/* <Typography>Laboris consectetur nisi mollit nostrud. Sint eu commodo consectetur irure labore sunt officia id incididunt fugiat. Eu anim ut minim ipsum sint fugiat tempor minim voluptate culpa velit. Tempor cupidatat in incididunt proident ut nulla veniam dolor do. Voluptate est cillum reprehenderit nisi occaecat culpa.
+      </Typography> */}
+
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main' , opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined  sx={{ fontSize:30 }}/>
+      </IconButton>
+    </JournalLayout>
   )
 }
